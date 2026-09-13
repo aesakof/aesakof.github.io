eb011614ad21a3daecb5c74b2899530d50dae898
aesakof <aesakof@gmail.com>
2026-09-13 19:01:30 -0400

    Updated NavBar for personal site

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -3,8 +3,13 @@
 import { useState } from "react"
 import Link from "next/link"
 import { usePathname, useRouter } from "next/navigation"
-import UserMenu from "./UserMenu"
 import { Menu, X } from "lucide-react"
+
+
+const links = [
+    { href: "/projects", label: "Projects" },
+    { href: "/about-me", label: "About Me"},
+]
 
 function MobileLink({href, onClick, children}: {href: string, onClick?: () => void, children:React.ReactNode}) {
     return (
@@ -17,33 +22,32 @@ function MobileLink({href, onClick, children}: {href: string, onClick?: () => vo
 export default function NavBar() {
     const [smallMenuOpen, setSmallMenuOpen] = useState(false)
     const pathname = usePathname()
-    const router = useRouter()
-
-    const publicLinks = [
-        { href: "/ui-samples", label: "UI Samples" }
-    ]
 
     const closeMenu = () => setSmallMenuOpen(false)
 
     return (
         <header className="bg-surface-raised border-b border-border text-text-primary p-2 relative">
             <div className="flex justify-between items-center">
-                <div className="flex">
-                    <Link href="/" className="px-4 py-2">[PROJECT LOGO HERE]</Link>
-                    <div className="md:flex hidden">
-                        {publicLinks.map(link => (
-                            <Link key={link.href} href={link.href} className="px-3 py-2 hover:bg-surface rounded-sm">
-                                {link.label}
-                            </Link>
-                        ))}
-                    </div>
-                </div>
-                <div className="md:flex items-center gap-4 hidden">
-
+                <Link href="/" className="px-4 py-2">Alex Esakof</Link>
+                <div className="md:flex hidden">
+                    {links.map(link => (
+                        <Link key={link.href} href={link.href} className="px-3 py-2 hover:bg-surface rounded-sm">
+                            {link.label}
+                        </Link>
+                    ))}
                 </div>
                 <button onClick={() => setSmallMenuOpen(!smallMenuOpen)} className="md:hidden flex px-4 py-2 hover:bg-surface rounded-sm">
                     {smallMenuOpen ? <X /> : <Menu />}
                 </button>
+                {smallMenuOpen &&
+                    <div className="md:hidden z-50 flex flex-col px-4 py-2 rounded-b-sm absolute top-full left-0 right-0 bg-surface border-b border-border">
+                        {links.map(link => (
+                            <MobileLink key={link.href} href={link.href}>
+                                {link.label}
+                            </MobileLink>
+                        ))}
+                    </div>
+                }
             </div>
         </header>
     )
